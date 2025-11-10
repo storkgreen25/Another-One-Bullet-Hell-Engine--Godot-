52e33a2738c7fe97e6238d016c13e5482b7b69aa
StorkGreen <139620501+storkgreen25@users.noreply.github.com>
2025-11-11 02:38:08 +0300

    Update spawner_projectiles.gd
    Updated the logic of overridden stats and speed change

--- spawners/spawner_projectiles.gd
+++ spawners/spawner_projectiles.gd
@@ -7,7 +7,8 @@ var custom_angle: float = 0.0
 var custom_rotation: float = 0.0
 var color_num: int = 0
 var color_cycle_num: int = 0
-var speed_change: int = 0
+var speed_change_type: int = 0
+var speed_change_layer: int = 0
 var distance_change: Vector2 = Vector2.ZERO
 
 func spawner_type_logic() -> void:
@@ -21,7 +22,7 @@ func spawner_type_logic() -> void:
 			if stats.cycle_in_type: color_control()
 			spawn()
 			custom_angle += stats.type_gap
-			speed_change += stats.type_speed_change
+			speed_change_type += stats.type_speed_change
 	#RING TYPE
 	elif stats.spawn_type == stats.TYPE.RING:
 		for y in stats.type_amount:
@@ -29,7 +30,7 @@ func spawner_type_logic() -> void:
 			if stats.cycle_in_type: color_control()
 			custom_angle += 360 / stats.type_amount + stats.type_gap
 			spawn()
-			speed_change += stats.type_speed_change
+			speed_change_type += stats.type_speed_change
 	#CORNER TYPE
 	elif stats.spawn_type == stats.TYPE.CORNER:
 		spawn()
@@ -62,7 +63,8 @@ func spawner_logic() -> void:
 			#STATS CHANGE AFTER SHOT
 			custom_angle = 0.0
 			distance_change += stats.distance_expand
-			speed_change += stats.layers_speed_change
+			speed_change_type = 0
+			speed_change_layer += stats.layers_speed_change
 			custom_rotation += stats.post_rotation
 			havent_shot = false
 			
@@ -71,15 +73,16 @@ func spawner_logic() -> void:
 	#RESETS STATS AFTER SHOT
 	distance_change = stats.distance_from_spawner
 	custom_rotation = 0.0
-	speed_change = 0
+	speed_change_layer = 0
 	havent_shot = true
 
 func instance_tweaking(instance: Node) -> void:
 	if stats.stats_override:
-		instance.stats = stats.stats_new
+		var new_stats: ProjectileStats = stats.stats_new.duplicate()
+		instance.stats = new_stats
 	
 	instance.position += stats.distance_from_spawner + distance_change
-	instance.stats.speed += speed_change
+	instance.stats.speed += speed_change_type + speed_change_layer
 	instance.rotation_degrees += custom_rotation + custom_angle
 	instance.change_color(color_num)
 	if stats.projectile_group != "": 
